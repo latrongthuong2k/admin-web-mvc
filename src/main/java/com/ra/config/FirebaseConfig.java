978f2a7c21e9f48a0ac9ac8776fc46e34bf6ce15
LA TRONG THUONG <74745602+latrongthuong2k@users.noreply.github.com>
2023-12-04 05:21:57 +0900

    fix path firebase in local server

--- src/main/java/com/ra/config/FirebaseConfig.java
+++ src/main/java/com/ra/config/FirebaseConfig.java
@@ -14,7 +14,7 @@ import java.nio.file.Paths;
 @Configuration
 public class FirebaseConfig {
 ///Users/latrongthuong/Downloads/DataSave/1-WorkSpace/git/ProjectModule04-main/build
-    private final String serviceAccountKeyPath = "/Users/latrongthuong/Downloads/DataSave/1-WorkSpace/git/ProjectModule04-main/src/main/webapp/resources/firebaseConfig.json";
+    private final String serviceAccountKeyPath = "/Users/latrongthuong/Downloads/1-WorkSpace/git/AdminWebMVC/src/main/webapp/resources/firebaseConfig.json";
 
     @Bean
     public Storage storage() throws IOException {
